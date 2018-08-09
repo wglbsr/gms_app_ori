2cda95b4e03f3523e07b43d7223e63c5942c6a5b
wglbs <wglbsr@163.com>
2018-08-09 21:37:17 +0800

    Initial commit

---
+++ src/components/home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <mt-header fixed title="长高-德远能源油机管理系统"></mt-header>
+
+    <mt-button class="gms-home-button">
+      <img slot="icon" src="../assets/images/icon-test.png">
+    </mt-button>
+
+    <mt-button class="gms-home-button">
+      <img slot="icon" src="../assets/images/icon-test.png">
+    </mt-button>
+    <mt-button class="gms-home-button">
+      <img slot="icon" src="../assets/images/icon-test.png">
+    </mt-button>    <mt-button class="gms-home-button">
+    <img slot="icon" src="../assets/images/icon-test.png">
+  </mt-button>    <mt-button class="gms-home-button">
+    <img slot="icon" src="../assets/images/icon-test.png">
+  </mt-button>    <mt-button class="gms-home-button">
+    <img slot="icon" src="../assets/images/icon-test.png">
+  </mt-button>    <mt-button class="gms-home-button">
+    <img slot="icon" src="../assets/images/icon-test.png">
+  </mt-button>
+
+    <mt-tabbar v-model="selected">
+      <mt-tab-item id="外卖">
+        <!--<img slot="icon" src="../assets/100x100.png">-->
+        操作
+      </mt-tab-item>
+      <mt-tab-item id="订单">
+        <!--<img slot="icon" src="../assets/100x100.png">-->
+        状态
+      </mt-tab-item>
+      <mt-tab-item id="发现">
+        <img slot="icon" src="../assets/images/icon-test.png">
+        报表
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        <i class="el-icon-share"></i>
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "home",
+    data() {
+      return {
+        selected: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .gms-home-button {
+    height: 100px;
+    width: 100px;
+    /*margin-left: 1rem;*/
+    /*margin-right: 1rem;*/
+    float: left;
+    margin: 1rem 1rem;
+  }
+
+</style>
